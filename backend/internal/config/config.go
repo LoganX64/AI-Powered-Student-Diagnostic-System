c46797238de7f4eba6fe800e6070855620b7fe42
LoganX64 <loganxtream@gmail.com>
2026-04-18 23:19:02 +0530

    feat(config): add dotenv support for environment variable loading

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -3,6 +3,8 @@ package config
 import (
 	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 type Config struct {
@@ -10,8 +12,14 @@ type Config struct {
 }
 
 func LoadConfig() *Config {
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found (this is fine in production)")
+	}
+
 	dbURL := os.Getenv("DB_URL")
-	log.Println("DB_URL from env:", os.Getenv("DB_URL"))
+	log.Println("DB_URL from env:", dbURL)
 
 	if dbURL == "" {
 		log.Fatal("DB_URL is not set")
